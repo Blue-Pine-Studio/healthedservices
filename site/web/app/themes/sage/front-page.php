f8d3d177ab445579ffc3348c54919ddc2e93c9c5
Michael Moffet <michael.moffet@plasticsurgerystudios.com>
2016-03-07 16:17:26 -0800

    front page images in place

--- site/web/app/themes/sage/front-page.php
+++ site/web/app/themes/sage/front-page.php
@@ -1,35 +1,58 @@
-    <!-- Main jumbotron for a primary marketing message or call to action -->
-    <div class="jumbotron">
-      <div class="container-fluid">
-        <h1>Hello, world!</h1>
-        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
-      </div>
+<!-- Main jumbotron for a primary marketing message or call to action -->
+<div class="jumbotron">
+  <div class="container-fluid">
+    <h1>Hello, world!</h1>
+    <h2>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</h2>
+    <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+  </div>
+</div>
+
+<div class="container-fluid">
+  <!-- Example row of columns -->
+  <div class="row">
+    <div class="class-button col-md-3">
+      <h2>Heading 1</h2>
     </div>
-
-    <div class="container-fluid">
-      <!-- Example row of columns -->
-      <div class="row">
-        <div class="col-md-4">
-          <h2>Heading 1</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-       </div>
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
+    <div class="class-button col-md-3">
+      <h2>Heading</h2>
+    </div>
+    <div class="class-button col-md-3">
+      <h2>Heading</h2>
+    </div>
+    <div class="class-button col-md-3">
+      <h2>Heading</h2>
+    </div>
+  </div>
+  <div class="row">
+    <section class="module content">
+      <div class="wrap">
+        <h2>Lorem Ipsum Dolor</h2>
+        <p>Bacon ipsum dolor amet beef jowl pork loin ham flank. Prosciutto capicola pig cupim biltong. Chicken frankfurter ball tip, landjaeger turkey filet mignon t-bone ham hock leberkas ham. Ball tip jerky tenderloin frankfurter swine ham hock kevin hamburger pork loin pork belly meatloaf shoulder. Bacon salami meatloaf shank, bresaola jowl kevin hamburger picanha. Corned beef beef ribs tri-tip turkey swine salami venison. Boudin hamburger filet mignon prosciutto, strip steak pastrami shankle.</p>
       </div>
+    </section>
 
-      <hr>
+    <section class="module parallax parallax-1">
+      <div class="wrap">
+      </div>
+    </section>
 
-      <footer>
-        <p>&copy; 2015 Company, Inc.</p>
-      </footer>
-    </div> <!-- /container -->
+    <section class="module content">
+      <div class="wrap">
+        <h2>Frequently Asked Questions</h2>
+        <p>Bacon ipsum dolor amet beef jowl pork loin ham flank. Prosciutto capicola pig cupim biltong. Chicken frankfurter ball tip, landjaeger turkey filet mignon t-bone ham hock leberkas ham. Ball tip jerky tenderloin frankfurter swine ham hock kevin hamburger pork loin pork belly meatloaf shoulder. Bacon salami meatloaf shank, bresaola jowl kevin hamburger picanha. Corned beef beef ribs tri-tip turkey swine salami venison. Boudin hamburger filet mignon prosciutto, strip steak pastrami shankle.</p>
+      </div>
+    </section>
+
+    <section class="module parallax parallax-1">
+      <div class="wrap">
+      </div>
+    </section>
+  </div>
+
+  <hr>
+
+  <footer>
+    <p>&copy; 2015 Company, Inc.</p>
+  </footer>
+</div>
+<!-- /container -->
--- site/web/app/themes/sage/templates/header.php
+++ site/web/app/themes/sage/templates/header.php
@@ -1,10 +1,12 @@
 <header class="banner">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="navbar navbar-inverse navbar-fixed-top">
+    <nav class="navbar navbar-inverse navbar-fixed-top navbar-right">
+      <a class="brand" href="<?= esc_url(home_url('/')); ?>">
+        <?php bloginfo('name'); ?>
+      </a>
       <?php
       if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-right']);
       endif;
       ?>
     </nav>
