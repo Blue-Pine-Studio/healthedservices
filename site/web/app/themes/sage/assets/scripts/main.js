16c8ca8565a84093318561699d419cae29b4c81e
Michael Moffet <michael.moffet@plasticsurgerystudios.com>
2016-03-14 12:02:58 -0700

    JS fix

--- site/web/app/themes/sage/assets/scripts/main.js
+++ site/web/app/themes/sage/assets/scripts/main.js
@@ -10,32 +10,32 @@
  * always reference jQuery with $, even when in .noConflict() mode.
  * ======================================================================== */
 
-(function($) {
+(function ($) {
 
   // Use this variable to set up the common and page specific functions. If you
   // rename this variable, you will also need to rename the namespace below.
   var Sage = {
     // All pages
     'common': {
-      init: function() {
+      init: function () {
         // JavaScript to be fired on all pages
       },
-      finalize: function() {
+      finalize: function () {
         // JavaScript to be fired on all pages, after page specific JS is fired
       }
     },
     // Home page
     'home': {
-      init: function() {
+      init: function () {
         // JavaScript to be fired on the home page
       },
-      finalize: function() {
+      finalize: function () {
         // JavaScript to be fired on the home page, after the init JS
       }
     },
     // About us page, note the change from about-us to about_us.
     'about_us': {
-      init: function() {
+      init: function () {
         // JavaScript to be fired on the about us page
       }
     }
@@ -44,7 +44,7 @@
   // The routing fires all common scripts, followed by the page specific scripts.
   // Add additional events for more control over timing e.g. a finalize event
   var UTIL = {
-    fire: function(func, funcname, args) {
+    fire: function (func, funcname, args) {
       var fire;
       var namespace = Sage;
       funcname = (funcname === undefined) ? 'init' : funcname;
@@ -56,12 +56,12 @@
         namespace[func][funcname](args);
       }
     },
-    loadEvents: function() {
+    loadEvents: function () {
       // Fire common init JS
       UTIL.fire('common');
 
       // Fire page-specific init JS, and then finalize JS
-      $.each(document.body.className.replace(/-/g, '_').split(/\s+/), function(i, classnm) {
+      $.each(document.body.className.replace(/-/g, '_').split(/\s+/), function (i, classnm) {
         UTIL.fire(classnm);
         UTIL.fire(classnm, 'finalize');
       });
@@ -79,73 +79,81 @@
 /** Document Ready Functions **/
 /********************************************************************/
 
-$( document ).ready(function() {
+$(document).ready(function () {
 
-    // Resive video
+  // Resive video
+  scaleVideoContainer();
+
+  initBannerVideoSize('.video-container .poster img');
+  initBannerVideoSize('.video-container .filter');
+  initBannerVideoSize('.video-container video');
+
+  $(window).on('resize', function () {
     scaleVideoContainer();
-
-    initBannerVideoSize('.video-container .poster img');
-    initBannerVideoSize('.video-container .filter');
-    initBannerVideoSize('.video-container video');
-
-    $(window).on('resize', function() {
-        scaleVideoContainer();
-        scaleBannerVideoSize('.video-container .poster img');
-        scaleBannerVideoSize('.video-container .filter');
-        scaleBannerVideoSize('.video-container video');
-    });
+    scaleBannerVideoSize('.video-container .poster img');
+    scaleBannerVideoSize('.video-container .filter');
+    scaleBannerVideoSize('.video-container video');
+  });
 
 });
 
 /** Reusable Functions **/
 /********************************************************************/
-
-function scaleVideoContainer() {
+(function ($) {
+  function scaleVideoContainer() {
 
     var height = ($(window).height()) * 0.63;
     var unitHeight = parseInt(height) + 'px';
-    $('.homepage-hero-module').css('height',unitHeight);
+    $('.homepage-hero-module').css('height', unitHeight);
 
-}
+  }
 
-function initBannerVideoSize(element){
+  function initBannerVideoSize(element) {
 
-    $(element).each(function(){
-        $(this).data('height', $(this).height());
-        $(this).data('width', $(this).width());
+    $(element).each(function () {
+      $(this).data('height', $(this).height());
+      $(this).data('width', $(this).width());
     });
 
     scaleBannerVideoSize(element);
 
-}
+  }
 
-function scaleBannerVideoSize(element){
+  function scaleBannerVideoSize(element) {
 
     var windowWidth = $(window).width(),
-        windowHeight = $(window).height(),
-        videoWidth,
-        videoHeight;
+      windowHeight = $(window).height(),
+      videoWidth,
+      videoHeight;
 
     console.log(windowHeight);
 
-    $(element).each(function(){
-        var videoAspectRatio = $(this).data('height')/$(this).data('width'),
-            windowAspectRatio = windowHeight/windowWidth;
+    $(element).each(function () {
+      var videoAspectRatio = $(this).data('height') / $(this).data('width'),
+        windowAspectRatio = windowHeight / windowWidth;
 
-        if (videoAspectRatio > windowAspectRatio) {
-            videoWidth = windowWidth;
-            videoHeight = videoWidth * videoAspectRatio;
-            $(this).css({'top' : -(videoHeight - windowHeight) / 2 + 'px', 'margin-left' : 0});
-        } else {
-            videoHeight = windowHeight;
-            videoWidth = videoHeight / videoAspectRatio;
-            $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
-        }
+      if (videoAspectRatio > windowAspectRatio) {
+        videoWidth = windowWidth;
+        videoHeight = videoWidth * videoAspectRatio;
+        $(this).css({
+          'top': -(videoHeight - windowHeight) / 2 + 'px',
+          'margin-left': 0
+        });
+      } else {
+        videoHeight = windowHeight;
+        videoWidth = videoHeight / videoAspectRatio;
+        $(this).css({
+          'margin-top': 0,
+          'margin-left': -(videoWidth - windowWidth) / 2 + 'px'
+        });
+      }
 
-        $(this).width(videoWidth).height(videoHeight);
+      $(this).width(videoWidth).height(videoHeight);
 
-        $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+      $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
 
 
     });
-}
+  }
+
+})(jQuery);
