e16343c5a3f7bd39b7819c10369aaf9a34c7a12a
Michael Moffet <michael.moffet@plasticsurgerystudios.com>
2016-05-02 07:43:29 -0700

    coming soon page added

--- site/web/app/themes/sage/template-custom.php
+++ site/web/app/themes/sage/template-custom.php
@@ -1,10 +1,81 @@
 <?php
 /**
- * Template Name: Custom Template
+ * Template Name: Home Page Template
  */
 ?>
 
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
-<?php endwhile; ?>
+<div class="homepage-hero-module">
+  <div class="video-container">
+    <div class="title-container">
+      <div class="headline">
+          <h1>Hello, world!</h1>
+      </div>
+      <div class="description">
+        <div class="inner">
+          <h2>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</h2>
+          <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+        </div>
+      </div>
+    </div>
+    <div class="filter"></div>
+    <video autoplay loop class="fillWidth">
+      <source src="<?= get_template_directory_uri(); ?>/dist/images/videos/cpr_video.webm" type="video/webm">
+      <source src="<?= get_template_directory_uri(); ?>/dist/images/videos/cpr_video.mp4" type="video/mp4">
+      <source src="<?= get_template_directory_uri(); ?>/dist/images/videos/cpr_video.ogv" type="video/ogg">
+    </video>
+    <div class="poster hidden">
+      <img src="<?= get_template_directory_uri(); ?>/dist/images/cpr_frame.jpg" alt="">
+    </div>
+  </div>
+</div>
+
+<div class="container-fluid">
+  <!-- Example row of columns -->
+  <div class="row">
+    <div class="class-button col-md-3">
+      <h2>Heading 1</h2>
+    </div>
+    <div class="class-button col-md-3">
+      <h2>Heading</h2>
+    </div>
+    <div class="class-button col-md-3">
+      <h2>Heading</h2>
+    </div>
+    <div class="class-button col-md-3">
+      <h2>Heading</h2>
+    </div>
+  </div>
+  <div class="row">
+    <section class="module content">
+      <div class="wrap">
+        <h2>Lorem Ipsum Dolor</h2>
+        <p>Bacon ipsum dolor amet beef jowl pork loin ham flank. Prosciutto capicola pig cupim biltong. Chicken frankfurter ball tip, landjaeger turkey filet mignon t-bone ham hock leberkas ham. Ball tip jerky tenderloin frankfurter swine ham hock kevin hamburger pork loin pork belly meatloaf shoulder. Bacon salami meatloaf shank, bresaola jowl kevin hamburger picanha. Corned beef beef ribs tri-tip turkey swine salami venison. Boudin hamburger filet mignon prosciutto, strip steak pastrami shankle.</p>
+      </div>
+    </section>
+
+    <section class="module parallax parallax-1">
+      <div class="wrap">
+      </div>
+    </section>
+
+    <section class="module content">
+      <div class="wrap">
+        <h2>Frequently Asked Questions</h2>
+        <p>Bacon ipsum dolor amet beef jowl pork loin ham flank. Prosciutto capicola pig cupim biltong. Chicken frankfurter ball tip, landjaeger turkey filet mignon t-bone ham hock leberkas ham. Ball tip jerky tenderloin frankfurter swine ham hock kevin hamburger pork loin pork belly meatloaf shoulder. Bacon salami meatloaf shank, bresaola jowl kevin hamburger picanha. Corned beef beef ribs tri-tip turkey swine salami venison. Boudin hamburger filet mignon prosciutto, strip steak pastrami shankle.</p>
+      </div>
+    </section>
+
+    <section class="module parallax parallax-1">
+      <div class="wrap">
+      </div>
+    </section>
+  </div>
+
+  <hr>
+
+  <footer>
+    <p>&copy; 2015 Company, Inc.</p>
+  </footer>
+</div>
+<!-- /container -->
+
